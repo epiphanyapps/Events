74565e7b3def425af3e2d72f64fbcf8adad3d29b
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2015-02-06 17:03:49 -0500

    initial commit

---
+++ Events/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Events
+//
+//  Created by Walter M Vargas-Pena on 2/6/15.
+//  Copyright (c) 2015 Epiphany Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'Events' do
+
+    pod "MagicalRecord", :git => 'https://github.com/magicalpanda/MagicalRecord.git', :branch => 'develop'
+    pod 'Parse'
+
+end
+
+target 'EventsTests' do
+
+end
+
