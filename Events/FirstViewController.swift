1f9b1e8bdb37abec3ebf9b0f6f6ef735e22064d7
Walter Martin Vargas-Pena <walter.v.vargas-pena@pfizer.com>
2015-04-26 04:52:23 -0400

    fresh swift project

---
+++ Events/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Events
+//
+//  Created by Walter Martin Vargas-Pena on 4/26/15.
+//  Copyright (c) 2015 Epiphany Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Events/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Events
+//
+//  Created by Walter Martin Vargas-Pena on 4/26/15.
+//  Copyright (c) 2015 Epiphany Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
